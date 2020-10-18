615b13c9c322ae3ed9adbea0d42dea9ad84cdd5b
Sezer BOZKIR <sezer.bozkir@huawei.com>
2020-10-18 23:38:05 +0300

    docker-compose add/remove/create functions added

--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 # Created by SezerBozkir<admin@sezerbozkir.com> at 10/18/2020
 import paramiko
 from paramiko import AutoAddPolicy
+import yaml
+import constants
 
 
 # paramiko.util.log_to_file('paramiko.log')
@@ -11,29 +13,30 @@ class SSHClient:
                  username="root",
                  password="1",
                  current_location='management'):
-        self.ip = ip
-        self.username = username
-        self.password = password
-        self.current_location = current_location
+        self._ip = ip
+        self._username = username
+        self._password = password
+        self._current_location = current_location
 
         self.client = paramiko.SSHClient()
         self.client.set_missing_host_key_policy(AutoAddPolicy())
         self.client.connect(hostname=ip,
                             username=username,
                             password=password)
-        self.sftp: paramiko.SFTPClient = self.client.open_sftp()
-        try:
-            self.sftp.stat(self.current_location)
-        except IOError as exception:
+        self._sftp: paramiko.SFTPClient = self.client.open_sftp()
+
+        if not self.sftp_exist(self._current_location):
             print(f"/root/{current_location} is not found. File creating.")
-            self.sftp.chdir('/root')
-            self.sftp.mkdir(self.current_location)
-        self.sftp.chdir("/root/" + self.current_location)
+            self._sftp.chdir('/root')
+            self._sftp.mkdir(self._current_location)
+
+        self._sftp.chdir("/root/" + self._current_location)
         self.check_and_install_docker()
+        self._compose_yaml = constants.compose_baseline
 
     def send_command(self, command, show_output=True):
         response = ""
-        stdin, stdout, stderr = self.client.exec_command(f"cd /root/{self.current_location}; {command}",
+        stdin, stdout, stderr = self.client.exec_command(f"cd /root/{self._current_location}; {command}",
                                                          get_pty=True)
         for line in iter(stdout.readline, ""):
             if show_output:
@@ -43,12 +46,14 @@ class SSHClient:
         return response
 
     def check_and_install_docker(self):
+        exist = True
         print("Docker status start to checking...")
         result = self.send_command("docker -v", show_output=False)
         if "Docker version" not in result:
             print("Docker is not installed. Installation started.")
             self.send_command("curl -fsSL https://get.docker.com -o get-docker.sh")
             self.send_command("sh get-docker.sh")
+            exist = False
         result = self.send_command("docker-compose -v", show_output=False)
         if "docker-compose version " not in result:
             print("Docker-compose is not installed. Installation started.")
@@ -56,11 +61,48 @@ class SSHClient:
                 """curl -L "https://github.com/docker/compose/releases/download/1.27.4/""" +
                 """docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose""")
             self.send_command("chmod +x /usr/local/bin/docker-compose")
-        print("Docker and/or docker-compose installed successfully.")
+            exist = False
+        if exist:
+            print("Docker and/or docker-compose already exist.Installation process skipping.")
+        else:
+            print("Docker and/or docker-compose installed successfully.")
 
     def change_current_directory(self, directory):
-        self.current_location = directory
-        self.sftp.chdir("/root/" + directory)
+        self._current_location = directory
+        self._sftp.chdir("/root/" + directory)
 
-    def create_directory(self, directory):
-        self.sftp.mkdir(self.current_location + "/" + directory)
+    def _create_directory(self, directory):
+        self._sftp.mkdir(self._current_location + "/" + directory)
+
+    def sftp_exist(self, path):
+        try:
+            self._sftp.stat(path)
+            return True
+        except FileNotFoundError:
+            return False
+
+    def create_yaml(self):
+        try:
+            with open("docker-compose.yml", "w") as file:
+                yaml.dump(self._compose_yaml, file, sort_keys=False)
+        except Exception as e:
+            print("Error while create docker-compose.yml", e)
+
+    def add_service(self, service: str):
+        if service.lower() == 'mysql':
+            self._compose_yaml['services'].update(constants.mysql)
+        elif service.lower() == 'redis':
+            print("Coming soon")
+        else:
+            print("This service not implemented yet.")
+
+    def remove_service(self, service):
+        if service in self._compose_yaml['services'].keys():
+            for service_name in constants.mysql.keys():
+                del self._compose_yaml['services'][service_name]
+        else:
+            print("Service not found")
+
+    def __del__(self):
+        print("Connection closed.")
+        self.client.close()
