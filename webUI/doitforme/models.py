6e38c4f210f1df3af40790140a51b8a7af979175
Sezer BOZKIR <sezer.bozkir@huawei.com>
2020-11-02 02:33:04 +0300

    Mock admin added. Servers table created

--- webUI/doitforme/models.py
+++ webUI/doitforme/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+
+class Servers(models.Model):
+    ip_address = models.CharField(max_length=30)
+    password = models.CharField(max_length=100)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'servers'
